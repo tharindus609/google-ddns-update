aa3be6da898e1d40ea02478ae6abdb837f05dfdc
tharindus609 <tharindus609@gmail.com>
2023-05-29 16:28:44 +0100

    remove custom query domain. extra info

--- gddns-updater.py
+++ gddns-updater.py
@@ -14,12 +14,11 @@ UPDATE_INTERVAL = 900 # 15 mins
 
 while True:
     DDNS_UPDATE_REQUIRED = False
-    q_domain = "query.{0}".format(g_domain)
 
-    print("querying current DDNS IP: {0}".format(q_domain))
+    print("querying current DDNS IP: {0}".format(g_domain))
 
     try:
-        sock_host = socket.gethostbyaddr(q_domain)
+        sock_host = socket.gethostbyaddr(g_domain)
 
         print("current DNS information")
         print("hostnames: {0}".format(sock_host[0]))
@@ -28,6 +27,7 @@ while True:
 
         if len(sock_host[2]) == 1:
             current_ddns_ip = sock_host[2][0]
+            print("current DDNS IP: {0}".format(current_ddns_ip))
         else:
             print("too may ips for: {0}".format(g_domain))
             print("terminating...")
@@ -46,7 +46,7 @@ while True:
             print("new public ip address detected")
             current_ddns_ip = gapi_response.text
             DDNS_UPDATE_REQUIRED = True
-            print('updated public IP address as: {0}'.format(current_ddns_ip))
+            print('updating new DDNS IP: {0}'.format(current_ddns_ip))
         else:
             DDNS_UPDATE_REQUIRED = False
             print("DDNS up-to-date. no DDNS update required")
@@ -57,5 +57,7 @@ while True:
         print("api response: {0}:{1}".format(gapi_response.status_code, gapi_response.reason))
         if gapi_response.status_code == 200:
             print("google DDNS records updated: {0}".format(gapi_response.text))
+        else:
+            print("Not OK response recived: {0}".format(gapi_response.text))
     
     sleep(UPDATE_INTERVAL)
